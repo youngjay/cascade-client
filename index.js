581ab5abf5cfdd5a8a8255232e5c02a7579e14d7
jay.yang <jay.yang@dianping.com>
2017-09-11 18:52:33 +0800

    dd

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ module.exports = {
     ErrorRejector: require('./lib/decorator/error-rejector'),
     Cacher: require('./lib/decorator/cacher'),
     Normalizer: require('./lib/decorator/normalizer'),
+    Logger: require('./lib/decorator/logger'),
+    Delayer: require('./lib/decorator/delayer'),
 
     // util
     getFieldAs: require('./lib/util/get-field-as')
